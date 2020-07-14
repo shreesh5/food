cefae6ef6a5705bdc022b55c047ec74c81ba6b46
Shreesh Nayak <shreeshnayak@gmail.com>
2020-07-14 11:50:15 -0700

    Removed yelp api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer pHZIHoQzbKAJMzIM_ztVRqpb41ZCPWhnKsw0LB5oPeK8y7aeZmwGUVHJ2jW45swnTMXv7M75yvifXaGRMXS_IoFVVV2so5gloldn1-qGLbk-clqGfnRqUrek-kALX3Yx'
+        Authorization: ''
     }
 });
